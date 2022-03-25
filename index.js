f8c460a9517ebc87eb10bec81a77b11a6d2a3d43
Trevoir Williams <trevoir.williams@gmail.com>
2022-03-24 23:47:09 -0500

    Added We Work Rmeotely

--- index.js
+++ index.js
@@ -12,27 +12,49 @@ app.get('/', (req, res) => {
 });
 
 app.get('/remotejobs', (req, res) => {
-    axios.get("https://stackoverflow.com/jobs?r=true").then((response) => {
+    const stackoverflowUrl = "https://stackoverflow.com";
+    axios.get(stackoverflowUrl + "/jobs?r=true")
+    .then((response) => {
         const page = response.data;
-        console.log(page);
         const $ = cheerio.load(page);
         
         $('[data-jobid]', page).each(function(){
             var joblink = $(this).find('div > div > div > h2 > a');
-            var image = $(this).find('img').attr('src');
-            var url = joblink.attr('href');
+            // var image = $(this).find('img').attr('src');
+            var url = stackoverflowUrl + joblink.attr('href');
             var company = $(this).find('div > div > div > h3 > span').first().text().trim();
             var title = joblink.text();
             jobs.push({
-                image,
+                // image,
                 url,
                 title,
                 company
             });
         });
-        res.json(jobs);
+        console.log(jobs);
+    }).then((response) => {
+        const weworkUrl = "https://weworkremotely.com";
+        axios.get(weworkUrl).then((response) => {
+            const page = response.data;
+            const $ = cheerio.load(page);
+            
+            $('li.feature', page).each(function(){
+                var joblink = $(this).find('a');
+                // var image = $(this).find('img').attr('src');
+                var url = weworkUrl + joblink.attr('href');
+                var company = joblink.find('span.company').text().trim();
+                var title = joblink.find('span.title').first().text().trim();
+                jobs.push({
+                    // image,
+                    url,
+                    title,
+                    company
+                });
+            });
+            console.log(jobs);
+            res.json(jobs);
+        });    
     });
-    
 });
 
 app.listen(PORT, () => {console.log(`Server running on ${PORT}`)})
